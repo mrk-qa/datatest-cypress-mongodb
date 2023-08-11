fb11c2f087928de13d9eefb24dd1527c3d6fbf0f
Marco Antonio <marco.ant.seg@gmail.com>
2023-08-11 15:28:34 -0300

    add custom commands queryMany

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,7 @@
 Cypress.Commands.add('queryOne', (query) => {
     cy.wrap(query).as('queryResult')
 })
+
+Cypress.Commands.add('queryMany', (query) => {
+    cy.findMany(query).as('queryResult')
+})
