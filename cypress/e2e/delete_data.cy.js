f2b5711c00e227b24ba5fbfc9364b4841d85efcc
Marco Antonio <marco.ant.seg@gmail.com>
2023-08-11 20:20:52 -0300

    passando options

--- cypress/e2e/delete_data.cy.js
+++ cypress/e2e/delete_data.cy.js
@@ -2,6 +2,7 @@ const { fakerPT_BR: faker } = require('@faker-js/faker')
 
 const name = faker.internet.userName().toLowerCase()
 const number = Math.floor(Math.random() * 1000)
+const options = { collection: 'types', database: 'pokemon' }
 
 describe('excluindo dados do DB', () => {
 
@@ -23,7 +24,7 @@ describe('excluindo dados do DB', () => {
     it('[delete] exclui um dado dentro da collection e do DB', () => {
         cy.section('deletar um')
         const indexNumber = number.toString()
-        cy.deleteOne({ index: indexNumber }).then(res => {
+        cy.deleteOne({ index: indexNumber }, options).then(res => {
             if (res) {
                 const data = JSON.stringify(res)
                 cy.task('log', data)
