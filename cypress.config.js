1417d11137e2b73a7077a7995f40faf932edcb80
Marco Antonio <marco.ant.seg@gmail.com>
2023-08-12 02:42:48 -0300

    ajuste path env

--- cypress.config.js
+++ cypress.config.js
@@ -1,35 +1,34 @@
-const { defineConfig } = require("cypress")
+const { defineConfig } = require('cypress')
 const allureWriter = require('@shelex/cypress-allure-plugin/writer')
 const mongo = require('cypress-mongodb')
 
 module.exports = defineConfig({
   projectId: 'eamhmz',
-  env: {
-    mongodb: {
-      uri: 'mongodb://mrk-qa:admin@localhost:27017/?authMechanism=DEFAULT',
-      collection: 'types',
-      database: 'pokemon',
-    },
-    allure: true,
-    allureResultsPath: 'cypress/reports/allure-results',
-    allureAttachRequests: true,
-    allureAddVideoOnPass: true,
-  },
   e2e: {
+    env: {
+      mongodb: {
+        uri: 'mongodb://mrk-qa:admin@localhost:27017/?authMechanism=DEFAULT',
+        collection: 'types',
+        database: 'pokemon',
+      },
+      allure: true,
+      allureResultsPath: 'cypress/reports/allure-results',
+      allureAttachRequests: true,
+      allureAddVideoOnPass: true,
+    },
     videosFolder: 'cypress/reports/videos',
     screenshotsFolder: 'cypress/reports/screenshots',
     responseTimeout: 10000,
-    video: false,
-    screenshotOnRunFailure: false,
     setupNodeEvents(on, config) {
-      mongo.configurePlugin(on);
-      allureWriter(on, config);
+      mongo.configurePlugin(on)
+      allureWriter(on, config)
       on('task', {
         log(message) {
           console.log(message)
           return null
         },
       })
+      return config
     },
   },
 })
