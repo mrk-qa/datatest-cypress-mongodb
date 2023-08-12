7bb68b429670ccf903733f4046d125420024d76b
Marco Antonio <marco.ant.seg@gmail.com>
2023-08-12 00:03:19 -0300

    add teste validação filtro danos vazios

--- cypress/e2e/query_data.cy.js
+++ cypress/e2e/query_data.cy.js
@@ -14,7 +14,7 @@ describe('consultando dados do DB', () => {
     cy.fixture('query/query_many_name.json').then(res => {
       const data = JSON.stringify(res)
       console.log(data)
-      expect(res[0].name).to.eq('Venusaur')
+      cy.wrap(res[0].name).should('contains', 'Venusaur')
     })
   })
 
@@ -26,7 +26,7 @@ describe('consultando dados do DB', () => {
 
     cy.section('dados esperados')
     cy.fixture('query/query_many_type_water.json').then(res => {
-      expect(res[0].pokemon_type1).to.equal('water')
+      cy.wrap(res[0].pokemon_type1).should('contains', 'water')
     })
   })
 
@@ -39,7 +39,44 @@ describe('consultando dados do DB', () => {
     cy.section('consultar')
     
     cy.findOne({ index: indexNumber }, options).then(res => {
-      expect(res).to.not.exist
+      cy.wrap(res).should('not.exist')
     })
   })
+
+  it('[query] consulta a quantidade de dados "vazios" na coluna "pokemon_type2"', () => {
+    cy.section('consultar e salvar a query')
+    cy.findMany({$or: [{ pokemon_type2: { $exists: false } }, { pokemon_type2: '' }]}, options).then(res => {
+      cy.writeFile('cypress/fixtures/query/query_many_null_pokemon_type2.json', res)
+    })
+
+    cy.section('dados esperados')
+    cy.fixture('query/query_many_null_pokemon_type2.json').then(res => {
+      cy.wrap(res).should('have.length', 249)
+    })
+  })
+
+  it('[query] consulta a quantidade de dados "unknown" na coluna "pokemon_type1"', () => {
+    cy.section('consultar e salvar a query')
+    cy.findMany({$or: [{ pokemon_type1: { $exists: false } }, { pokemon_type1: 'unknown' }]}, options).then(res => {
+      cy.writeFile('cypress/fixtures/query/query_many_unknown_pokemon_type1.json', res)
+    })
+
+    cy.section('dados esperados')
+    cy.fixture('query/query_many_unknown_pokemon_type1.json').then(res => {
+      cy.wrap(res).should('have.length', 5)
+    })
+  })
+
+  it('[query] consulta a quantidade de dados "vazios" na coluna "index"', () => {
+    cy.section('consultar e salvar a query')
+    cy.findMany({$or: [{ index: { $exists: false } }, { index: '' }]}, options).then(res => {
+      cy.writeFile('cypress/fixtures/query/query_many_null_index.json', res)
+    })
+
+    cy.section('dados esperados')
+    cy.fixture('query/query_many_null_index.json').then(res => {
+      cy.wrap(res).should('have.length', 5)
+    })
+  })
+
 })
