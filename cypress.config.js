89d8c114b0ff1ff926211f940302567a7ace9044
Marco Antonio <marco.ant.seg@gmail.com>
2023-08-12 01:21:13 -0300

    add lib allure report

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("cypress")
+const allureWriter = require('@shelex/cypress-allure-plugin/writer')
 const mongo = require('cypress-mongodb')
 
 module.exports = defineConfig({
@@ -9,13 +10,20 @@ module.exports = defineConfig({
       collection: 'types',
       database: 'pokemon',
     },
+    allure: true,
+    allureResultsPath: 'cypress/reports/allure-results',
+    allureAttachRequests: true,
+    allureAddVideoOnPass: true,
   },
   e2e: {
+    videosFolder: 'cypress/reports/videos',
+    screenshotsFolder: 'cypress/reports/screenshots',
     responseTimeout: 10000,
     video: false,
     screenshotOnRunFailure: false,
     setupNodeEvents(on, config) {
       mongo.configurePlugin(on);
+      allureWriter(on, config);
       on('task', {
         log(message) {
           console.log(message)
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,5 +1,6 @@
 import './commands'
 import 'cypress-plugin-steps'
+import '@shelex/cypress-allure-plugin'
 
 const mongo = require('cypress-mongodb')
 mongo.addCommands()
