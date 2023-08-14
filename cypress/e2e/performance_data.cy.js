005eefa73563e6572c34c6f74f550eb20942cd83
Marco Antonio <marco.ant.seg@gmail.com>
2023-08-13 22:21:07 -0300

    add teste de performance da query

---
+++ cypress/e2e/performance_data.cy.js
@@ -0,0 +1,22 @@
+const options = { collection: Cypress.env('mongodb').collection, database: Cypress.env('mongodb').database }
+
+describe('Performance dos dados ao DB', () => {
+
+  it('[performance] tempo de resposta de uma query em ms', () => {
+    const startTime = Date.now()
+
+    cy.wrap(new Promise((resolve) => {
+      cy.findMany({$or: [{ pokemon_type2: { $exists: false } }, { pokemon_type2: '' }]}, options).then(() => {
+        const endTime = Date.now()
+
+        const elapsedTime = endTime - startTime
+
+        cy.log(`Tempo da query: ${elapsedTime} ms`)
+        expect(elapsedTime).to.be.lessThan(200)
+
+        resolve()
+      })
+    }))
+  })
+
+})
