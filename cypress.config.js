e42efaefbea52cb62509265dedb393bd09d4d2b9
Marco Antonio <marco.ant.seg@gmail.com>
2023-08-14 20:12:56 -0300

    add specPattern

--- cypress.config.js
+++ cypress.config.js
@@ -16,6 +16,14 @@ module.exports = defineConfig({
       allureAttachRequests: true,
       allureAddVideoOnPass: true,
     },
+    specPattern: [
+      "cypress/e2e/create_*.cy.js",
+      "cypress/e2e/delete_*.cy.js",
+      "cypress/e2e/query_*.cy.js",
+      "cypress/e2e/update_*.cy.js",
+      "cypress/e2e/performance_*.cy.js",
+      "cypress/e2e/migration_*.cy.js"
+    ],
     videosFolder: 'cypress/reports/videos',
     screenshotsFolder: 'cypress/reports/screenshots',
     responseTimeout: 10000,
